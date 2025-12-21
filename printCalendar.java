8b4363512ac488791270508b37f82db3366850fe
BurakHINGE <burakmentese16@gmail.com>
2025-12-21 19:33:26 +0300

    Update printCalendar.java
    Bug has been fixed.

--- printCalendar.java
+++ printCalendar.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class printcalendar {
+public class printCalendar {
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
